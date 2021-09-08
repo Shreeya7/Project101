d08d997a0c5e76aac66039218082c428c541eaa9
R.Shreeya <73177260+Shreeya7@users.noreply.github.com>
2021-09-08 18:58:06 +0530

    Add files via upload

---
+++ CloudStorage.py
@@ -0,0 +1,32 @@
+import dropbox
+import os 
+from dropbox.files import WriteMode
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self,file_from,file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+    
+        for root,dirs,files in os.walk(file_from):
+            for filename in files:
+                local_path = os.path.join(root,filename)
+
+                relative_path = os.path.relpath(local_path,file_from)
+                dropbox_path = os.path.join(file_to,relative_path)
+
+                with open(local_path ,'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path , mode=WriteMode('overwrite'))
+
+def main():
+    access_token = 'GxwnxgAh8EUAAAAAAAAAAXMsazACpH6U65mN0mdO7jiSE1aDUiikJ0YIKz0VPx-J'
+    transferData = TransferData(access_token)
+
+    file_from = input("Enter the file path to transfer : ")
+    file_to = input("Enter the path to upload to Dropbox : ")
+
+    transferData.upload_file(file_from,file_to)
+    print("File has been uploaded")
+
+main()
